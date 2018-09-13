bd398156df98dcf6ed8cf777c6118384fd8de766
RachelQiu <saro_qiu@163.com>
2018-09-13 22:31:28 +0800

    首页 Header 修改成 无状态组件

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {connect} from 'react-redux';
 import {
     HeaderWrapper,
     Login,
@@ -10,25 +11,52 @@ import {
 } from './style';
 
 
-class Header extends Component{
-    render(){
-        return (
-            <HeaderWrapper>
-                <Login href="#" />
-                <Nav>
-                    <NavItem className="left active">首页</NavItem>
-                    <NavItem className="left">下载App</NavItem>
-                    <NavItem className="right">登录</NavItem>
-                    <NavItem className="right">Aa</NavItem>
-                    <NavSearch></NavSearch>
-                </Nav>
-                <Addition>
-                    <Button className="writing">写文章</Button>
-                    <Button className="red">注册</Button>
-                </Addition>
-            </HeaderWrapper>
-          );
+const Header =(props)=>{
+    return(
+        <HeaderWrapper>
+            <Login href="#" />
+            <Nav>
+                <NavItem className="left active">首页</NavItem>
+                <NavItem className="left">下载App</NavItem>
+                <NavItem className="right">登录</NavItem>
+                <NavItem className="right">Aa</NavItem>
+                <NavSearch 
+                    className={props.focused?'focused':''}
+                    onFocus={props.handleInputFocus}
+                    onBlur={props.handleInputBlur}
+                >
+                </NavSearch>
+            </Nav>
+            <Addition>
+                <Button className="writing">写文章</Button>
+                <Button className="red">注册</Button>
+            </Addition>
+        </HeaderWrapper>
+    );
+}
+
+
+const mapStateToPros=(state)=>{
+    return {
+        focused:state.focused
     }
 }
 
-export default Header;
+const mapDispathToProps=(dispatch)=>{
+    return {
+        handleInputFocus(){
+            const action = {
+                type:'search_focused'
+            }
+            dispatch(action);
+        },
+        handleInputBlur(){
+            const action = {
+                type:'search_blur'
+            }
+            dispatch(action);
+        }
+    }
+}
+
+export default connect(mapStateToPros,mapDispathToProps)(Header);
